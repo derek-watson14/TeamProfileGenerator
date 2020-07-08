c781622938d427f6bbec249c10ca8530754ec0a5
Derek Watson <derek.watson92@gmail.com>
2020-07-07 21:31:28 -0700

    added write file functionality

--- app.js
+++ app.js
@@ -10,25 +10,23 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 const employees = [];
-const generalQuestions = [
-  {
-    type: "input",
-    message: "Name:",
-    name: "name",
-  },
-  {
-    type: "input",
-    message: "Email:",
-    name: "email",
-  },
-];
 
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
 const generateId = () => Math.random().toString(36).substr(2, 9);
 
 const askEmployeeInfo = async (uniqueQuestion) => {
-  const questions = [...generalQuestions, uniqueQuestion];
+  const questions = [
+    {
+      type: "input",
+      message: "Name:",
+      name: "name",
+    },
+    {
+      type: "input",
+      message: "Email:",
+      name: "email",
+    },
+    uniqueQuestion,
+  ];
   const info = await inquirer.prompt(questions);
   info.id = generateId();
   return info;
@@ -64,6 +62,15 @@ const addIntern = async () => {
   employees.push(new Intern(name, id, email, school));
 };
 
+const makeHtmlFile = (html) => {
+  fs.mkdir(OUTPUT_DIR, { recursive: true }, (err) => {
+    if (err) throw err;
+  });
+  fs.writeFile(outputPath, html, (err) => {
+    if (err) throw err;
+  });
+};
+
 const mainMenu = async () => {
   const menu = await inquirer.prompt([
     {
@@ -75,7 +82,9 @@ const mainMenu = async () => {
   ]);
 
   if (menu.choice === "Done") {
-    console.log("Rendering pages...");
+    console.log("Rendering HTML...");
+    const html = render(employees);
+    makeHtmlFile(html);
   } else if (menu.choice === "Add Manager") {
     await addManager();
     mainMenu();
@@ -89,23 +98,3 @@ const mainMenu = async () => {
 };
 
 mainMenu();
-
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
