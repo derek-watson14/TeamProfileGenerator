342ca2e5ae333fc18c31d30fe7b990ab2be75890
Derek Watson <derek.watson92@gmail.com>
2020-07-07 21:11:01 -0700

    wrote prompts to add employees to list

--- app.js
+++ app.js
@@ -9,10 +9,86 @@ const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
-
+const employees = [];
+const generalQuestions = [
+  {
+    type: "input",
+    message: "Name:",
+    name: "name",
+  },
+  {
+    type: "input",
+    message: "Email:",
+    name: "email",
+  },
+];
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
+const generateId = () => Math.random().toString(36).substr(2, 9);
+
+const askEmployeeInfo = async (uniqueQuestion) => {
+  const questions = [...generalQuestions, uniqueQuestion];
+  const info = await inquirer.prompt(questions);
+  info.id = generateId();
+  return info;
+};
+
+const addManager = async () => {
+  const managerQ = {
+    type: "number",
+    message: "Office Number:",
+    name: "officeNumber",
+  };
+  const { name, id, email, officeNumber } = await askEmployeeInfo(managerQ);
+  employees.push(new Manager(name, id, email, officeNumber));
+};
+
+const addEngineer = async () => {
+  const engineerQ = {
+    type: "input",
+    message: "Github Username:",
+    name: "github",
+  };
+  const { name, id, email, github } = await askEmployeeInfo(engineerQ);
+  employees.push(new Engineer(name, id, email, github));
+};
+
+const addIntern = async () => {
+  const internQ = {
+    type: "number",
+    message: "School:",
+    name: "school",
+  };
+  const { name, id, email, school } = await askEmployeeInfo(internQ);
+  employees.push(new Intern(name, id, email, school));
+};
+
+const mainMenu = async () => {
+  const menu = await inquirer.prompt([
+    {
+      type: "list",
+      message: "What would you like to do?",
+      name: "choice",
+      choices: ["Add Manager", "Add Engineer", "Add Intern", "Done"],
+    },
+  ]);
+
+  if (menu.choice === "Done") {
+    console.log("Rendering pages...");
+  } else if (menu.choice === "Add Manager") {
+    await addManager();
+    mainMenu();
+  } else if (menu.choice === "Add Engineer") {
+    await addEngineer();
+    mainMenu();
+  } else if (menu.choice === "Add Intern") {
+    await addIntern();
+    mainMenu();
+  }
+};
+
+mainMenu();
 
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
